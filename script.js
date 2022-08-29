004e8e833422b20386848375c27fdc1623fd0f68
Hanno Pleis <hanno@ulfertsprygoda.de>
2022-08-29 11:30:04 +0200

    create functional method

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World!")
+components = {
+    signs   : ["sun", "moon", "aries"],
+    lucks   : ["excellent", "good", "horrible"],
+    advices : ["trust no one", "keep your promises", "stop procrastinating"],
+}
+
+createMessage = () => {
+
+    for(attr in components) {
+        switch (attr) {
+            case "signs":
+                console.log(`Your sign is ${components.signs[Math.floor(Math.random() * 3)]}.`)
+                break;
+            case "lucks": 
+                console.log(`You are having ${components.lucks[Math.floor(Math.random() * 3)]} luck.`);
+                break;
+            case "advices":
+                console.log(`You should ${components.advices[Math.floor(Math.random() * 3)]}.`)
+                break;
+        }
+    }
+    
+;
+}
+
+createMessage();
